164638e0819af654e01a6b60cc9c5a6142a4ec77
Marieme Sougoufara <mariemesgpro@gmail.com>
2024-04-12 09:02:44 +0000

    adding javadocComments

--- geoanalytique/model/Triangle.java
+++ geoanalytique/model/Triangle.java
@@ -1,28 +1,67 @@
-package model;
-import model.Point;
-import model.GeoObject;
+package geoanalytique.model;
 
-public interface Triangle extends GeoObject {
+/**
+ * Classe abstraite représentant un triangle dans un espace géométrique.
+ */
+public abstract class Triangle {
+    /** Le premier sommet du triangle. */
+    protected Point sommet1;
+    /** Le deuxième sommet du triangle. */
+    protected Point sommet2;
+    /** Le troisième sommet du triangle. */
+    protected Point sommet3;
 
-    // Retourne et définit le premier point du triangle.
-    Point getSommet1();
-    void setSommet1(Point sommet1);
+    /**
+     * Constructeur pour initialiser les sommets du triangle.
+     *
+     * @param sommet1 Le premier sommet du triangle.
+     * @param sommet2 Le deuxième sommet du triangle.
+     * @param sommet3 Le troisième sommet du triangle.
+     */
+    public Triangle(Point sommet1, Point sommet2, Point sommet3) {
+        this.sommet1 = sommet1;
+        this.sommet2 = sommet2;
+        this.sommet3 = sommet3;
+    }
 
-    // Retourne et définit le deuxième point du triangle.
-    Point getSommet2();
-    void setSommet2(Point sommet2);
+    /**
+     * Méthode abstraite pour calculer le périmètre du triangle.
+     *
+     * @return Le périmètre du triangle.
+     */
+    public abstract double calculerPerimetre();
 
-    // Retourne et définit le troisième point du triangle.
-    Point getSommet3();
-    void setSommet3(Point sommet3);
+    /**
+     * Méthode abstraite pour calculer l'aire du triangle.
+     *
+     * @return L'aire du triangle.
+     */
+    public abstract double calculerAire();
 
-    // Calcule le périmètre du triangle.
-    double getPerimetre();
+    /**
+     * Méthode concrète pour obtenir le premier sommet du triangle.
+     *
+     * @return Le premier sommet du triangle.
+     */
+    public Point getSommet1() {
+        return sommet1;
+    }
 
-    // Calcule l'aire du triangle.
-    double getAire();
+    /**
+     * Méthode concrète pour obtenir le deuxième sommet du triangle.
+     *
+     * @return Le deuxième sommet du triangle.
+     */
+    public Point getSommet2() {
+        return sommet2;
+    }
 
-    // Détermine si un point est à l'intérieur du triangle.
-    boolean estPointInterieur(Point point);
-
+    /**
+     * Méthode concrète pour obtenir le troisième sommet du triangle.
+     *
+     * @return Le troisième sommet du triangle.
+     */
+    public Point getSommet3() {
+        return sommet3;
+    }
 }
