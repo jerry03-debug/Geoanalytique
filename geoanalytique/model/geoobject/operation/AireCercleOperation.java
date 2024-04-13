026dbd3cfc9e8f8a04799f67094a6c0d89f072d8
DabaSD <dabasambdiagne30@gmail.com>
2024-04-13 00:52:18 +0000

    Nouvelle version

--- geoanalytique/model/geoobject/operation/AireCercleOperation.java
+++ geoanalytique/model/geoobject/operation/AireCercleOperation.java
@@ -3,5 +3,46 @@ package geoanalytique.model.geoobject.operation;
 import geoanalytique.util.Operation;
 
 public class AireCercleOperation implements Operation {
+
+    private float rayon;
+
+    public AireCercleOperation(float rayon) {
+        this.rayon = rayon;
+    }
+
+    @Override
+    public Double calculer() {
+        return Math.PI * Math.pow(rayon, 2);
+    }
+
+    @Override
+    public String getTitle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTitle'");
+    }
+
+    @Override
+    public int getArite() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getArite'");
+    }
+
+    @Override
+    public void setArgument(int num, Object o) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setArgument'");
+    }
+
+    @Override
+    public Class getClassArgument(int num) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getClassArgument'");
+    }
+
+    @Override
+    public void changerNom(String nouveauNom) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'changerNom'");
+    }
     
 }
--- geoanalytique/model/geoobject/operation/DeplacerEllipseOperation.java
+++ geoanalytique/model/geoobject/operation/DeplacerEllipseOperation.java
@@ -3,5 +3,48 @@ package geoanalytique.model.geoobject.operation;
 import geoanalytique.util.Operation;
 
 public class DeplacerEllipseOperation implements Operation {
+        
+        private float x;
+        private float y;
+        
+        public DeplacerEllipseOperation(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+        
+        @Override
+        public Double calculer() {
+            return null;
+        }
+        
+        @Override
+        public String getTitle() {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'getTitle'");
+        }
+        
+        @Override
+        public int getArite() {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'getArite'");
+        }
+        
+        @Override
+        public void setArgument(int num, Object o) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'setArgument'");
+        }
+        
+        @Override
+        public Class getClassArgument(int num) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'getClassArgument'");
+        }
+        
+        @Override
+        public void changerNom(String nouveauNom) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'changerNom'");
+        }
     
 }
