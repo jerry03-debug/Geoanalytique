bc7c4a1048f6cd35db86d3006072518419f24946
emmanuelakdg <akdgemman@gmail.com>
2024-04-16 01:01:24 +0000

    correction du repere

--- geoanalytique/controleur/GeoAnalytiqueControleur.java
+++ geoanalytique/controleur/GeoAnalytiqueControleur.java
@@ -29,31 +29,81 @@ public class GeoAnalytiqueControleur {
         drawYAxis(g, width, height);
     }
 
+    // private void drawXAxis(Graphics g, int width, int height) {
+    //     g.setColor(Color.BLACK);
+
+    //     // Dessiner l'axe des X
+    //     g.drawLine(AXIS_MARGIN, height / 2, width - AXIS_MARGIN, height / 2);
+
+    //     // Dessiner les graduations de l'axe des X
+    //     for (int x = AXIS_MARGIN + TICK_SPACING; x < width - AXIS_MARGIN; x += TICK_SPACING) {
+    //         if (x != width / 2) { // Exclure le zéro
+    //             g.drawLine(x, height / 2 - TICK_SIZE / 2, x, height / 2 + TICK_SIZE / 2);
+    //             g.drawString(Integer.toString((x - width / 2) / TICK_SPACING), x - TICK_LABEL_MARGIN, height / 2 - TICK_LABEL_MARGIN);
+    //         }
+    //     }
+    // }
+
+    // private void drawYAxis(Graphics g, int width, int height) {
+    //     g.setColor(Color.BLACK);
+
+    //     // Dessiner l'axe des Y
+    //     g.drawLine(width / 2, AXIS_MARGIN, width / 2, height - AXIS_MARGIN);
+
+    //     // Dessiner les graduations de l'axe des Y
+    //     for (int y = AXIS_MARGIN + TICK_SPACING; y < height - AXIS_MARGIN; y += TICK_SPACING) {
+    //         if (y != height / 2) { // Exclure le zéro
+    //             g.drawLine(width / 2 - TICK_SIZE / 2, y, width / 2 + TICK_SIZE / 2, y);
+    //             g.drawString(Integer.toString((height / 2 - y) / TICK_SPACING), width / 2 + TICK_LABEL_MARGIN, y + TICK_LABEL_MARGIN);
+    //         }
+    //     }
+    // }
+
     private void drawXAxis(Graphics g, int width, int height) {
-        g.setColor(Color.BLACK);
+    g.setColor(Color.BLACK);
 
-        // Dessiner l'axe des X
-        g.drawLine(AXIS_MARGIN, height / 2, width - AXIS_MARGIN, height / 2);
+    // Dessiner l'axe des X
+    g.drawLine(AXIS_MARGIN, height / 2, width - AXIS_MARGIN, height / 2);
 
-        // Dessiner les graduations de l'axe des X
-        for (int x = AXIS_MARGIN + TICK_SPACING; x < width - AXIS_MARGIN; x += TICK_SPACING) {
-            g.drawLine(x, height / 2 - TICK_SIZE / 2, x, height / 2 + TICK_SIZE / 2);
-            g.drawString(Integer.toString((x - width / 2) / TICK_SPACING), x - TICK_LABEL_MARGIN, height / 2 - TICK_LABEL_MARGIN);
-        }
+    // Dessiner les graduations de l'axe des X pour les valeurs positives
+    int graduationValue = 1;
+    for (int x = width / 2 + TICK_SPACING; x < width - AXIS_MARGIN; x += TICK_SPACING) {
+        g.drawLine(x, height / 2 - TICK_SIZE / 2, x, height / 2 + TICK_SIZE / 2);
+        g.drawString(Integer.toString(graduationValue), x - TICK_LABEL_MARGIN, height / 2 - TICK_LABEL_MARGIN);
+        graduationValue++;
     }
 
-    private void drawYAxis(Graphics g, int width, int height) {
-        g.setColor(Color.BLACK);
-
-        // Dessiner l'axe des Y
-        g.drawLine(width / 2, AXIS_MARGIN, width / 2, height - AXIS_MARGIN);
-
-        // Dessiner les graduations de l'axe des Y
-        for (int y = AXIS_MARGIN + TICK_SPACING; y < height - AXIS_MARGIN; y += TICK_SPACING) {
-            g.drawLine(width / 2 - TICK_SIZE / 2, y, width / 2 + TICK_SIZE / 2, y);
-            g.drawString(Integer.toString((height / 2 - y) / TICK_SPACING), width / 2 + TICK_LABEL_MARGIN, y + TICK_LABEL_MARGIN);
-        }
+    // Dessiner les graduations de l'axe des X pour les valeurs négatives
+    graduationValue = -1;
+    for (int x = width / 2 - TICK_SPACING; x > AXIS_MARGIN; x -= TICK_SPACING) {
+        g.drawLine(x, height / 2 - TICK_SIZE / 2, x, height / 2 + TICK_SIZE / 2);
+        g.drawString(Integer.toString(graduationValue), x - TICK_LABEL_MARGIN, height / 2 - TICK_LABEL_MARGIN);
+        graduationValue--;
     }
+}
+
+private void drawYAxis(Graphics g, int width, int height) {
+    g.setColor(Color.BLACK);
+
+    // Dessiner l'axe des Y
+    g.drawLine(width / 2, AXIS_MARGIN, width / 2, height - AXIS_MARGIN);
+
+    // Dessiner les graduations de l'axe des Y pour les valeurs positives
+    int graduationValue = 1;
+    for (int y = height / 2 - TICK_SPACING; y > AXIS_MARGIN; y -= TICK_SPACING) {
+        g.drawLine(width / 2 - TICK_SIZE / 2, y, width / 2 + TICK_SIZE / 2, y);
+        g.drawString(Integer.toString(graduationValue), width / 2 + TICK_LABEL_MARGIN, y + TICK_LABEL_MARGIN);
+        graduationValue++;
+    }
+
+    // Dessiner les graduations de l'axe des Y pour les valeurs négatives
+    graduationValue = -1;
+    for (int y = height / 2 + TICK_SPACING; y < height - AXIS_MARGIN; y += TICK_SPACING) {
+        g.drawLine(width / 2 - TICK_SIZE / 2, y, width / 2 + TICK_SIZE / 2, y);
+        g.drawString(Integer.toString(graduationValue), width / 2 + TICK_LABEL_MARGIN, y + TICK_LABEL_MARGIN);
+        graduationValue--;
+    }
+}
 
 
     // Méthode pour ajouter un objet graphique correspondant à un GeoObject à la liste
